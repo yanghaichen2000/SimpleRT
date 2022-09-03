3cb99074b7d54652202ec91037fa21e227ab67c6
yhc233 <46913940+yanghaichen2000@users.noreply.github.com>
2022-09-04 02:36:07 +0800

    实现基于ggx和lambertian的非金属材质

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@ using std::cout;
 // 12. btdf
 // 13. 体积
 // 14. 透明材质						完成（）
+// 15. 降噪
+// 16. ggx非金属材质
 
 
 #define do_render
@@ -37,7 +39,7 @@ int main()
 	const double aspect_ratio = 1; // 16.0 / 9.0
 	const int image_width = 1024; //800
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 8;
+	const int samples_per_pixel = 512;
 	const int max_depth = 5;
 
 	// 打开图像文件
@@ -64,10 +66,10 @@ int main()
 	shared_ptr<material> green = make_shared<phong_material>(vec3(0, 0.8, 0), vec3(0, 0, 0));
 	shared_ptr<material> phong_2 = make_shared<phong_material>(vec3(1.0, 1.0, 1.0), vec3(0, 0, 0));
 	shared_ptr<material> white = make_shared<phong_material>(vec3(1, 1, 1), vec3(0, 0, 0));
-	shared_ptr<material> gold = make_shared<ggx_material>(0.2);
-	shared_ptr<material> stone = make_shared<ggx_material>(0.8, vec3(0.04, 0.04, 0.04));
-	shared_ptr<material> aluminum = make_shared<ggx_material>(0.003, vec3(0.939, 0.914, 0.822));
-	shared_ptr<material> copper = make_shared<ggx_material>(0.6, vec3(0.904, 0.372, 0.256));
+	shared_ptr<material> gold = make_shared<ggx_metal_material>(0.2);
+	shared_ptr<material> stone = make_shared<ggx_metal_material>(0.8, vec3(0.04, 0.04, 0.04));
+	shared_ptr<material> aluminum = make_shared<ggx_metal_material>(0.003, vec3(0.939, 0.914, 0.822));
+	shared_ptr<material> copper = make_shared<ggx_metal_material>(0.6, vec3(0.904, 0.372, 0.256));
 
 	// 场景顶点
 	vec3 vertex_1(-2, -2, -8);
@@ -78,10 +80,10 @@ int main()
 	vec3 vertex_6(2, 2, -8);
 	vec3 vertex_7(2, 2, -4);
 	vec3 vertex_8(-2, 2, -4);
-	vec3 light_vertex_1 = vec3(-0.5, 1.999, -6.5);
-	vec3 light_vertex_2 = vec3(0.5, 1.999, -6.5);
-	vec3 light_vertex_3 = vec3(0.5, 1.999, -5.5);
-	vec3 light_vertex_4 = vec3(-0.5, 1.999, -5.5);
+	vec3 light_vertex_1 = vec3(-1, 1.999, -6.25);
+	vec3 light_vertex_2 = vec3(1, 1.999, -6.25);
+	vec3 light_vertex_3 = vec3(1, 1.999, -5.75);
+	vec3 light_vertex_4 = vec3(-1, 1.999, -5.75);
 
 	// 三角形实例
 	auto box_material = white;
@@ -104,7 +106,9 @@ int main()
 
 	// 球
 	//world.add(make_shared<sphere>(point3(0.8, -1.2, -5.2), 0.79, aluminum));
-	//world.add(make_shared<sphere>(point3(-1.3, 1.3, -7), 0.6, aluminum));
+	shared_ptr<texture> tex_sphere = make_shared<simple_color_texture>(vec3(1, 1, 0.1));
+	shared_ptr<material> mat_sphere = make_shared<ggx_nonmetal_material>(0.02, 0.08, tex_sphere);
+	world.add(make_shared<sphere>(point3(-1.3, 0.2, -7.3), 0.7, mat_sphere));
 	
 	// cornell box
 	world.add(make_shared<triangle>(tri1));
@@ -127,10 +131,11 @@ int main()
 	shared_ptr<texture> tex_cube = make_shared<color_map>("obj/honey_d.jpg");
 	shared_ptr<texture> tex_cube_n = make_shared<normal_map>("obj/honey_n.jpg");
 	shared_ptr<texture> tex_bunny = make_shared<simple_color_texture>(vec3(1, 1, 1));
+	//shared_ptr<texture> tex_bunny = make_shared<simple_color_texture>(vec3(1, 0.782, 0.344));
 	
-	shared_ptr<material> mat_cow = make_shared<phong_material>(20, tex_cow);
-	shared_ptr<material> mat_cube = make_shared<ggx_material>(0.1, tex_cube, vec3(0, 0, 0), tex_cube_n);
-	shared_ptr<material> mat_bunny = make_shared<phong_material>(tex_bunny);
+	shared_ptr<material> mat_cow = make_shared<ggx_nonmetal_material>(0.05, 0.058, tex_cow);
+	shared_ptr<material> mat_cube = make_shared<ggx_metal_material>(0.1, tex_cube, vec3(0, 0, 0), tex_cube_n);
+	shared_ptr<material> mat_bunny = make_shared<ggx_nonmetal_material>(0.1, 0.08, tex_bunny);
 
 	shared_ptr<material> mat_default = make_shared<phong_material>(vec3(1, 1, 1));
 
@@ -161,9 +166,9 @@ int main()
 	shared_ptr<light> light_ptr_3 = make_shared<triangle_light>(light_3); // 三角形光源
 
 	vector<shared_ptr<light>> light_ptr_list;
-	light_ptr_list.push_back(light_ptr_1);
-	//light_ptr_list.push_back(light_ptr_2);
-	//light_ptr_list.push_back(light_ptr_3);
+	//light_ptr_list.push_back(light_ptr_1);
+	light_ptr_list.push_back(light_ptr_2);
+	light_ptr_list.push_back(light_ptr_3);
 
 	// 设置camera
 	camera cam(aspect_ratio);
--- src/material.h
+++ src/material.h
@@ -34,8 +34,8 @@ public:
 	vec3 radiance; // 自发光强度
 	shared_ptr<texture> color_map_ptr = nullptr;
 	shared_ptr<texture> normal_map_ptr = nullptr;
-	vec3 kd = vec3(0.9, 0.9, 0.9); // 漫反射系数
-	vec3 ks = vec3(0.1, 0.1, 0.1); // 镜面反射系数
+	double kd = 0.9; // 漫反射系数
+	double ks = 0.1; // 镜面反射系数
 	double a = 0; // 高光参数，越大则高光越小。默认a = 0，即漫反射材质
 
 public:
@@ -73,30 +73,58 @@ public:
 
 	// www.cs.princeton.edu/courses/archive/fall08/cos526/assign3/lawrence.pdf
 	virtual double sample_wi(vec3 &wi, const vec3 &wo, const vec3 &normal) const override {
-		/*
-		// 在上半球均匀采样
-		wi = unit_vector(random_in_hemisphere(normal));
-		// pdf为常数
-		return pi2_inv;
-		*/
+		
+		// 混合cos-weighted采样和高光项重要性采样
+		// 两种采样的频率的比值等于kd与ks之比
+		double pdf;
 
-		// cos-weighted采样
-		double rand1 = random_double();
-		double rand2 = random_double();
+		if (random_double() < kd / (kd + ks)) {
 
-		double theta = acos(sqrt(1 - rand1));
-		double phi = pi2 * rand2;
+			// cos-weighted采样
+			double rand1 = random_double();
+			double rand2 = random_double();
 
-		// 利用法线构建坐标系
-		vec3 b1, b2;
-		build_basis(normal, b1, b2);
+			double theta = acos(sqrt(1 - rand1));
+			double phi = pi2 * rand2;
 
-		// 得到wi
-		wi = normal * cos(theta) + b1 * sin(phi) * sin(theta) + b2 * cos(phi) * sin(theta);
+			// 利用法线构建坐标系
+			vec3 b1, b2;
+			build_basis(normal, b1, b2);
 
-		double pdf = cos(theta) * pi_inv;
+			// 得到wi
+			wi = normal * cos(theta) + b1 * sin(phi) * sin(theta) + b2 * cos(phi) * sin(theta);
+
+			pdf = cos(theta) * pi_inv;
+		}
+		else {
+
+			// 高光项重要性采样
+			double rand1 = random_double();
+			double rand2 = random_double();
+
+			// 随机采样半程向量
+			double theta = acos(pow(rand1, 1.0 / (a + 1)));
+			double phi = pi2 * rand2;
+			
+			vec3 b1, b2;
+			build_basis(normal, b1, b2);
+
+			vec3 h = normal * cos(theta) + b1 * sin(phi) * sin(theta) + b2 * cos(phi) * sin(theta);
+			
+			// 根据镜面反射计算wi
+			wi = 2 * h * dot(wo, h) - wo;
+
+			// 如果光线穿透，返回一个很大的pdf使该结果无贡献
+			if (dot(wi, normal) < 0) {
+				return 1000000;
+			}
+
+			// 计算pdf
+			pdf = (a + 1) * pi2_inv * pow(cos(theta), a);
+		}
 
 		return pdf;
+		
 	}
 
 	// phong brdf
@@ -104,7 +132,7 @@ public:
 	virtual vec3 brdf(const vec3 &wi, const vec3 &wo, const vec3 &normal, const vec3 &uv) const override {
 		//auto ret1 = color_map_ptr->get_value(uv) * pi_inv;
 		vec3 ret_d = color_map_ptr->get_value(uv) * kd * pi_inv;
-		vec3 ret_s = ks * (a + 2) * pi2_inv * pow(std::max(0.0, dot(normal, unit_vector(wi + wo))), a);
+		vec3 ret_s = vec3(ks, ks, ks) * (a + 2) * pi2_inv * pow(std::max(0.0, dot(normal, unit_vector(wi + wo))), a);
 
 		return ret_d + ret_s;
 	}
@@ -123,8 +151,8 @@ public:
 };
 
 
-// GGX材质
-class ggx_material : public material {
+// GGX金属材质
+class ggx_metal_material : public material {
 public:
 	double a; // 粗糙度，[0,1]
 	vec3 radiance; // 自发光强度
@@ -133,59 +161,122 @@ public:
 
 public:
 	// 用一种颜色构造material
-	ggx_material(const double &a_init, const vec3 &F0_init = vec3(1, 0.582, 0.0956), vec3 radiance_init = vec3(0.0, 0.0, 0.0)) {
+	ggx_metal_material(const double &a_init, const vec3 &F0_init = vec3(1, 0.582, 0.0956), vec3 radiance_init = vec3(0.0, 0.0, 0.0)) {
 		a = a_init;
 		color_map_ptr = make_shared<simple_color_texture>(F0_init);
 		radiance = radiance_init;
 	}
 
 	// 用color_map构造material，同时也支持法线贴图
-	ggx_material(const double &a_init, shared_ptr<texture> color_map_ptr_init, vec3 radiance_init = vec3(0.0, 0.0, 0.0), shared_ptr<texture> normal_map_ptr_init = nullptr) {
+	ggx_metal_material(const double &a_init, shared_ptr<texture> color_map_ptr_init, vec3 radiance_init = vec3(0.0, 0.0, 0.0), shared_ptr<texture> normal_map_ptr_init = nullptr) {
 		
 		a = a_init;
+		radiance = radiance_init;
 		color_map_ptr = color_map_ptr_init;
 		normal_map_ptr = normal_map_ptr_init;
 	}
 
 	// 必须输入单位向量!!!!!!!!
 	virtual double sample_wi(vec3 &wi, const vec3 &wo, const vec3 &normal) const override {
-		/*
-		// 在上半球均匀采样
-		wi = unit_vector(random_in_hemisphere(normal));
-		// pdf为常数
-		return pi2_inv;
-		*/
-		
-		if (random_double() > 0)
-		{
+
+		// 重要性采样微表面法线，使用的分布为ggx NDF
+		double rand1, rand2, theta, phi;
+		vec3 h;
+
+		rand1 = random_double();
+		rand2 = random_double();
+		theta = atan(a * sqrt(rand1 / (1 - rand1))); // 微观法线与宏观法线的夹角
+		phi = pi2 * rand2; // 微观法线在切平面的角度
+
+		// 利用法线构建坐标系
+		vec3 b1, b2;
+		build_basis(normal, b1, b2);
+
+		// 得到微观法线(即wi和wo的半程向量)
+		h = normal * cos(theta) + b1 * sin(phi) * sin(theta) + b2 * cos(phi) * sin(theta); // 弄错了一次，注意。
+
+		// 根据镜面反射计算wi
+		wi = 2 * h * dot(wo, h) - wo;
+
+		// 如果光线穿透，返回一个很大的pdf使该结果无贡献
+		if (dot(wi, normal) < 0) {
+			return 10000;
+		}
+
+		// pdf由NDF推出
+		double a2 = a * a;
+		double pdf = a2 * cos(theta) * pi_inv / pow(pow(cos(theta), 2) * (a2 - 1) + 1, 2) * 0.25 / dot(wo, h);
+
+		return pdf;
+	}
+
+	// 必须输入单位向量!!!!!!!!
+	virtual vec3 brdf(const vec3 &wi, const vec3 &wo, const vec3 &normal, const vec3 &uv) const override {
+		// F 菲涅尔项
+		vec3 h = unit_vector(wi + wo);
+		vec3 F = color_map_ptr->get_value(uv) + (vec3(1.0, 1.0, 1.0) - color_map_ptr->get_value(uv)) * pow(1 - dot(wo, h), 5);
+
+		// D NDF项
+		double a2 = a * a;
+		double D = a2 / (pi * pow(pow(dot(normal, h), 2) * (a2 - 1) + 1, 2));
+
+		// G 几何项（考虑遮挡）
+		double dot_n_v = dot(normal, wo);
+		double dot_n_l = dot(normal, wi);
+		double k = a * 0.5;
+		double G = dot_n_v / (dot_n_v * (1 - k) + k) * dot_n_l / (dot_n_l * (1 - k) + k);
+
+		return D * F * G / (4 * dot_n_v * dot_n_l);
+	}
+
+	virtual vec3 get_radiance() const override {
+		return radiance;
+	}
+
+	virtual shared_ptr<texture> get_normal_map_ptr() const override {
+		return normal_map_ptr;
+	}
+
+	virtual shared_ptr<texture> get_color_map_ptr() const override {
+		return color_map_ptr;
+	}
+};
+
+// GGX非金属材质
+class ggx_nonmetal_material : public material {
+public:
+	double a; // 高光项粗糙度，[0,1]
+	vec3 radiance; // 自发光强度
+	double F0; // 菲涅尔系数，决定了高光项和漫反射项的比例
+	shared_ptr<texture> color_map_ptr = nullptr; // 漫反射项颜色
+	shared_ptr<texture> normal_map_ptr = nullptr;
+
+public:
+	// 用color_map构造material，同时也支持法线贴图
+	ggx_nonmetal_material(const double &a_init, const double &F0_init, shared_ptr<texture> color_map_ptr_init, vec3 radiance_init = vec3(0.0, 0.0, 0.0), shared_ptr<texture> normal_map_ptr_init = nullptr) {
+
+		a = a_init;
+		F0 = F0_init;
+		radiance = radiance_init;
+		color_map_ptr = color_map_ptr_init;
+		normal_map_ptr = normal_map_ptr_init;
+	}
+
+	// 接下来需要完成brdf和采样函数
+
+	// 必须输入单位向量!!!!!!!!
+	// 随机使用ggx重要性采样或漫反射重要性采样（cos-weighted采样）
+	virtual double sample_wi(vec3 &wi, const vec3 &wo, const vec3 &normal) const override {
+
+		double pdf;
+
+		// 两种采样方法的权重根据菲涅尔项决定，因为它代表了高光项和漫反射项的能量比例
+		if (random_double() < F0) {
+			
 			// 重要性采样微表面法线，使用的分布为ggx NDF
 			double rand1, rand2, theta, phi;
 			vec3 h;
 
-			/*
-			bool wi_ok = false;
-			while (!wi_ok) {
-				// 首先计算局部坐标的theta和phi
-				rand1 = random_double();
-				rand2 = random_double();
-				theta = atan(a * sqrt(rand1 / (1 - rand1))); // 微观法线与宏观法线的夹角
-				phi = pi2 * rand2; // 微观法线在切平面的角度
-
-				// 利用法线构建坐标系
-				vec3 b1, b2;
-				build_basis(normal, b1, b2);
-
-				// 得到微观法线(即wi和wo的半程向量)
-				h = normal * cos(theta) + b1 * sin(phi) * sin(theta) + b2 * cos(phi) * sin(theta); // 弄错了一次，注意。
-
-				// 根据镜面反射计算wi
-				wi = 2 * h * dot(wo, h) - wo;
-
-				if (dot(wi, normal) > 0) {
-					wi_ok = true;
-				}
-			}
-			*/
 			rand1 = random_double();
 			rand2 = random_double();
 			theta = atan(a * sqrt(rand1 / (1 - rand1))); // 微观法线与宏观法线的夹角
@@ -208,12 +299,10 @@ public:
 
 			// pdf由NDF推出
 			double a2 = a * a;
-			double pdf = a2 * cos(theta) * pi_inv / pow(pow(cos(theta), 2) * (a2 - 1) + 1, 2) * 0.25 / dot(wo, h);
-
-			return pdf;
+			pdf = a2 * cos(theta) * pi_inv / pow(pow(cos(theta), 2) * (a2 - 1) + 1, 2) * 0.25 / dot(wo, h);
 		}
-		else
-		{
+		else {
+			
 			// cos-weighted采样
 			double rand1 = random_double();
 			double rand2 = random_double();
@@ -228,29 +317,39 @@ public:
 			// 得到wi
 			wi = normal * cos(theta) + b1 * sin(phi) * sin(theta) + b2 * cos(phi) * sin(theta);
 
-			double pdf = cos(theta) * pi_inv;
-
-			return pdf;
+			pdf = cos(theta) * pi_inv;
 		}
+
+		return pdf;
 	}
 
 	// 必须输入单位向量!!!!!!!!
+	// brdf是漫反射项与高光项的和
 	virtual vec3 brdf(const vec3 &wi, const vec3 &wo, const vec3 &normal, const vec3 &uv) const override {
-		// F 菲涅尔项
-		vec3 h = unit_vector(wi + wo);
-		vec3 F = color_map_ptr->get_value(uv) + (vec3(1.0, 1.0, 1.0) - color_map_ptr->get_value(uv)) * pow(1 - dot(wo, h), 5);
 
+		// 高光项（使用ggx模型）
+
+		// F 菲涅尔项
+		// 对于非金属，菲涅尔项的RGB值相等
+		vec3 h = unit_vector(wi + wo);
+		double F_value = F0 + (1.0 - F0) * pow(1 - dot(wo, h), 5);
+		vec3 F(F_value, F_value, F_value);
 		// D NDF项
 		double a2 = a * a;
 		double D = a2 / (pi * pow(pow(dot(normal, h), 2) * (a2 - 1) + 1, 2));
-
 		// G 几何项（考虑遮挡）
 		double dot_n_v = dot(normal, wo);
 		double dot_n_l = dot(normal, wi);
 		double k = a * 0.5;
 		double G = dot_n_v / (dot_n_v * (1 - k) + k) * dot_n_l / (dot_n_l * (1 - k) + k);
 
-		return D * F * G / (4 * dot_n_v * dot_n_l);
+		vec3 component_ggx = F * D * G / (4 * dot_n_v * dot_n_l);
+		component_ggx = clamp(component_ggx, 0, 1);
+
+		// 漫反射项
+		vec3 component_diffuse = (vec3(1 - F_value, 1 - F_value, 1 - F_value)) * color_map_ptr->get_value(uv) * pi_inv;
+
+		return component_ggx + component_diffuse;
 	}
 
 	virtual vec3 get_radiance() const override {
